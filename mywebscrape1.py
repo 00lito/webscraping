dd434da2c67dc1865b5b2bd21f978fbc70a1c2ff
lito <emelito@gmail.com>
2022-06-21 19:39:15 -0700

    updated web scraping to use selenium

--- mywebscrape1.py
+++ mywebscrape1.py
@@ -1,8 +1,15 @@
-import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-page = requests.get('https://www.imdb.com/chart/top/')  # get page HTML through request
-soup = BeautifulSoup(page.content, 'html.parser')  # parse content using beautifulsoup
+option = webdriver.ChromeOptions()
+option.add_argument('--no-sandbox')
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.imdb.com/chart/top/")  # get page HTML through request
+soup = BeautifulSoup(driver.page_source, 'html.parser')  # parse content using beautifulsoup
 
 links = soup.select("table tbody tr td.titleColumn a")  # select all the anchors with titles
 first10 = links[:10]  # keep the first 10 anchors
